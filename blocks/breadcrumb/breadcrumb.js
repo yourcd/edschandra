44e6cb166620927812019e8edfd88fbc77ce9f59
Chandrashekhar Deshmukh <cdeshmukh@adobe.com>
2026-08-24 18:30:41 +0530

    updated breadcrumb to display dynamic path

--- blocks/breadcrumb/breadcrumb.js
+++ blocks/breadcrumb/breadcrumb.js
@@ -1,31 +1,63 @@
 import { getMetadata } from '../../scripts/lib-franklin.js';
 
-/**
- *
- * @param {HTMLElement} $block The main element
- */
-export default function decorate($block) {
-  const title = getMetadata('og:title');
-  const $ul = document.createElement('ul');
-  $block.append($ul);
-  const trail = [{
-    text: 'Adventures',
-    link: '/adventures',
-  }, {
-    text: title,
-  }];
-  while (trail.length) {
-    const step = trail.shift();
-    const $li = document.createElement('li');
-    $ul.append($li);
-    let $wrap = $li;
+export default function decorate(block) {
+  const title = getMetadata('og:title') || document.title;
+  const pathname = window.location.pathname;
+
+console.log('###pathname:='+pathname);
+  const parts = pathname
+    .split('/')
+    .filter(Boolean);
+
+  const trail = [
+    {
+      text: 'Home',
+      link: '/',
+    },
+  ];
+
+  let currentPath = '';
+
+  parts.forEach((part, index) => {
+    currentPath += `/${part}`;
+
+    const isLast = index === parts.length - 1;
+
+    trail.push({
+      text: isLast ? title : formatLabel(part),
+      link: isLast ? null : currentPath,
+    });
+  });
+
+  const ul = document.createElement('ul');
+
+  trail.forEach((step) => {
+    const li = document.createElement('li');
+
     if (step.link) {
-      $wrap = document.createElement('a');
-      $wrap.href = step.link;
-      $li.append($wrap);
+      const link = document.createElement('a');
+      link.href = step.link;
+
+      const span = document.createElement('span');
+      span.textContent = step.text;
+
+      link.append(span);
+      li.append(link);
+    } else {
+      const span = document.createElement('span');
+      span.textContent = step.text;
+      li.append(span);
     }
-    const $span = document.createElement('span');
-    $wrap.append($span);
-    $span.textContent = step.text;
-  }
+
+    ul.append(li);
+  });
+
+  block.append(ul);
+}
+
+function formatLabel(value) {
+	console.log('In FormatLabel function:='+value);
+  return decodeURIComponent(value)
+    .replace(/[-_]+/g, ' ')
+    .replace(/\b\w/g, (letter) => letter.toUpperCase());
 }
